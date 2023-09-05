d1c244e2f7685a48c27a6e9bdaf362d19e8ee1e2
Shu12388y <spl.sp999@gmail.com>
2023-09-06 01:01:00 +0530

    frontend file added

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import HeroPage from "../components/HeroPage";
+import MainPage from "../components/MainPage";
+import Card from "../components/Card";
+import LowerPage from "../components/LowerPage";
+function Home() {
+  return (
+    <>
+      <HeroPage />
+      <MainPage />
+      <Card />
+      <LowerPage />
+    </>
+  );
+}
+
+export default Home;
